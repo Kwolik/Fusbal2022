a37399024bc71ace28a9b32525bb7044f9b9d7b9
Robert Kwoll <robert.kwoll@itsilesia.com>
2022-08-06 18:39:51 +0200

    Change avatar profile

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -4,16 +4,21 @@ import {
   TextInput,
   TouchableOpacity,
   Text,
+  Image,
+  Button,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { auth, db, firestore } from "../components/firebase";
 import { doc, setDoc, getDoc } from "firebase/firestore";
+import { Avatar } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
 
 const SettingsScreen = () => {
   const [id, setId] = useState();
   const [nameUser, setNameUser] = useState("");
   const [email, setEmail] = useState("");
   const [nick, setNick] = useState("");
+  const [photo, setPhoto] = useState("");
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -28,7 +33,7 @@ const SettingsScreen = () => {
               setNameUser(doc.data().name);
               setEmail(doc.data().email);
               setNick(doc.data().nick);
-              console.log("Document data:", doc.data());
+              setPhoto(doc.data().photo);
             } else {
               console.log("No such document!");
             }
@@ -60,9 +65,43 @@ const SettingsScreen = () => {
     firestore.collection("users").doc(id).update({ nick: nick });
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      base64: true,
+      allowsEditing: true,
+      aspect: [2, 2],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      const base = `data:image/jpg;base64,${result.base64}`;
+      setPhoto(base);
+
+      firestore
+        .collection("users")
+        .doc(id)
+        .update({
+          photo: base,
+        }) //Poprawic catch pozniej
+        .catch((error) => alert("Zbyt duze zdj zmniejsz je!"));
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.email}>{email}</Text>
+      {photo ? (
+        <Image
+          style={{ width: "40%", height: "20%" }}
+          source={{
+            uri: photo,
+          }}
+        />
+      ) : (
+        <Avatar.Text size={124} label={nameUser.substring(0, 1)} />
+      )}
       <TextInput
         placeholder="Name"
         value={nameUser}
@@ -82,6 +121,8 @@ const SettingsScreen = () => {
       <TouchableOpacity onPress={changeNick} style={styles.button}>
         <Text style={styles.buttonText}>Change Nick</Text>
       </TouchableOpacity>
+
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
     </View>
   );
 };
