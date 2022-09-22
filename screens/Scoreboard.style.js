652a83fbcba10a49bfb583678b2b53323d46562e
Robert Kwoll <robert.kwoll@itsilesia.com>
2022-09-22 13:46:44 +0200

    Create Scoreboard

---
+++ screens/Scoreboard.style.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#0D4A85", //do zmiany jak doda sie svg
+  },
+  title: {
+    fontFamily: "Work-Sans",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 21,
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    marginTop: 80,
+    color: "#FFFFFF",
+  },
+  ranked: {
+    flexDirection: "row",
+    marginTop: 12,
+  },
+  numberOne: {
+    alignItems: "center",
+  },
+  numberTwo: {
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginRight: -8,
+  },
+  numberThree: {
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginLeft: -12,
+  },
+  number: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 10,
+    letterSpacing: 0.17,
+    color: "#FFFFFF",
+  },
+  avatar: {
+    width: 88,
+    height: 88,
+    borderRadius: 44,
+  },
+  avatar2: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+  },
+  name: {
+    fontFamily: "Work-Sans",
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 10,
+    letterSpacing: 0.17,
+    color: "rgba(0, 0, 0, 0.87)",
+    width: 64,
+    textAlign: "center",
+  },
+  punctation: {
+    fontFamily: "Work-Sans",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 11,
+    letterSpacing: 0.17,
+    color: "rgba(0, 0, 0, 0.87)",
+  },
+  flatlist: {
+    width: "86%",
+    height: "54%",
+    marginTop: 20,
+  },
+});
+
+export default styles;
